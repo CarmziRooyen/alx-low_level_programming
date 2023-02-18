07dabf9ca23864b051645689dcdda134591336d3
CarmziRooyen <carmzi.stokes@gmail.com>
2023-02-17 16:21:26 -0800

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <studio.h>
+/**
+ * main - Main block
+ *Description: Print alphabet in lower case
+ * Return: Always 0
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++
+	}
+	c = 'A';
+
+	while (c <= 'Z')
+		putchar(C);
+		C++
+	}
+
+	putchar('\n');
+	return (0);
