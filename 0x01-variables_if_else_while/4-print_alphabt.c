4b81b8f555033033aa6586cebf3e88184c58d1d1
CarmziRooyen <carmzi.stokes@gmail.com>
2023-02-19 03:52:17 -0800

    5-print_numbers.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
  * main - Main block
- * Description: Get a random number, and check its
- * Return: Always 0
+ *
+ * Return: 0 (Success)
  */
 int main(void)
 {
@@ -10,12 +10,13 @@ int main(void)
 
 	while (c <= 'z')
 	{
-		if(c != 'e' && c != 'q')
+		if (c != 'e' && c != 'q')
 		{
 			putchar(c);
 		}
 		c++;
-	{
-	putchar('\n');
+	}
+	putchar ('\n');
 	return (0);
 }
+
