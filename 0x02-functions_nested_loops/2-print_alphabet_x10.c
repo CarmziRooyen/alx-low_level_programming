379740455953836b8cc2298a1d17d09bd2284c2f
CarmziRooyen <carmzi.stokes@gmail.com>
2023-02-22 14:01:13 -0800

    update

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * print_alphabey_x10 -  Print 10 times the alphabet in lowercase
+ * print_alphabet_x10 -  Print 10 times the alphabet in lowercase
  * followed by a new line
  */
-void print_alphabey_x10(void)
+void print_alphabet_x10(void)
 {
 	char ch;
 	int i;
